3a15d42d24f04d75f79defeb7846cec8a35e4604
ferruzzi <ferruzzi@amazon.com>
2022-12-02 23:22:56 -0800

    Added new day file template

---
+++ Day 0/solution.py
@@ -0,0 +1,12 @@
+# Link to puzzle: https://adventofcode.com/2022/day/
+from utils import provide_inputs
+
+
+@provide_inputs
+def solve(inputs):
+    # TODO: add day number to end of link on line 1
+    ...
+
+
+if __name__ == "__main__":
+    solve()
